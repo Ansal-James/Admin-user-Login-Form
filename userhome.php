d1981b85b75cc00c4461b3675376be8ec9a8f9df
Ansal James <ansaljame45@gmail.com>
2024-10-13 19:14:14 +0530

    created a Admin-user-Login-Form

---
+++ userhome.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+if(!isset($_SESSION["username"]))
+{
+    header("location:login.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <H1>This is user Home</H1><?php echo $_SESSION["username"] ?>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
